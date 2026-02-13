3cec33b9509e76d72a99e76707d9cf39a501a079
Nicolas Marcelino <nicolasmarcelino1212@gmail.com>
2026-02-13 20:24:36 -0300

    exercicio 1051 imposto de renda

--- 1051 Imposto de renda.py
+++ 1051 Imposto de renda.py
@@ -1,30 +1,22 @@
-sal = float(input())
+a=float(input())
 
-if sal >= 0 and sal <= 2000.00:
+if(0<a<=2000):
     print("Isento")
-    exit()
-    
-if sal >= 2000.01 and sal <= 3000.00:
-    salisento = sal - 2000
-    salnovo = (salisento*0.08)
-    print(f"R$ {salnovo:.2f}")
-    
-<<<<<<< HEAD
-if sal >= 3000.01 and sal <= 4500.00:
-    salisento = sal - 2000
-    salbaixo = (salisento - 1000)
-    salmedio = ((salbaixo*0.18) + (salisento*0.08))
-    print(f"R$ {salmedio}")
-=======
-if sal >= 3000.01 and sal >= 4500.00:
-    salposto = sal - 2000
-    salnovo = (sal*0.18)
-    print(f"R$ {salnovo}")
-    exit()
-    
-if sal >= 45000.00:
-    salposto = sal - 2000
-    salnovo = (sal*0.28)
-    print(f"R$ {salnovo}")
-    exit()
->>>>>>> 7cc2143c5903b5f691a797ca2c8897ac659018ca
+elif(2000<a<=3000):
+    t= (a-2000)
+    tx= (t*8)/100
+    print("R$ %.2f"%tx)
+elif(3000<a<=4500):
+    t= (a-2000)
+    t1=t-1000
+    tx1=(1000*8)/100
+    tx2= (t1*18)/100
+    print("R$ %.2f"%(tx1+tx2))
+else:
+    t= (a-2000)
+    t1= t-1000
+    tx1= (1000*8)/100
+    t2=t1-1500
+    tx2=(1500*18)/100
+    tx= (t2*28)/100
+    print("R$ %.2f" %(tx+tx1+tx2))
