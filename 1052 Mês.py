0769c8812b3909a56128529e089b0313b1cc61e5
Nicolas Marcelino <nicolas.marcelino@exata.it>
2026-02-13 20:47:38 -0300

    exercicio 1051 Imposto de renda.py

---
+++ 1052 Mês.py
@@ -0,0 +1,26 @@
+a=int(input())
+
+if a == 1:
+    print("Janeiro")
+if a == 2:
+    print("Fevereiro")
+if a == 3:
+    print("Marco")
+if a == 4:
+    print("Abril")
+if a == 5:
+    print("Maio")
+if a == 6:
+    print("Junho")
+if a == 7:
+    print("Julho")
+if a == 8:
+    print("Agosto")
+if a == 9:
+    print("Setembro")
+if a == 10:
+    print("Outubro")
+if a == 11:
+    print("Novembro")
+if a == 12:
+    print("Dezembro")
