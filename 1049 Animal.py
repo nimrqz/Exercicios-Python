c7096206f5bdc91c5f83b168c55e7b057ef239c1
Nicolas Marcelino <nicolasmarcelino1212@gmail.com>
2026-02-13 16:10:38 -0300

    Add files via upload

---
+++ 1049 Animal.py
@@ -0,0 +1,15 @@
+p1 = input()
+p2 = input()
+p3 = input()
+
+if p1 == ("vertebrado"):
+    if p2 == ("ave"):
+        if p3 == ("carnivoro"):
+            print("aguia")
+        elif p3 == ("onivoro"):
+            print("pomba")
+    elif p2 == ("mamifero"):
+        if p3 == ("onivoro"):
+            print("homem")
+        elif p3 == ("herbivoro"):
+            print("vaca")
