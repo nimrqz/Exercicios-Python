c622d2306eb4c83afb821cec10edae874c1f1815
Nicolas Marcelino <nicolasmarcelino1212@gmail.com>
2026-02-13 16:28:20 -0300

    exercicio 1050 ddd

---
+++ 1050 DDD.py
@@ -0,0 +1,28 @@
+entrada = int(input())
+
+if entrada == 61:
+    print("Brasilia")
+    exit()
+if entrada == 71:
+    print("Salvador")
+    exit()
+if entrada == 11:
+    print("Sao Paulo")
+    exit()
+if entrada == 21:
+    print("Rio de Janeiro")
+    exit()
+if entrada == 32:
+    print("Juiz de Fora")
+    exit()
+if entrada == 19:
+    print("Campinas")
+    exit()
+if entrada == 27:
+    print("Vitoria")
+    exit()
+if entrada == 31:
+    print("Belo Horizonte")
+    exit()
+else:
+    print("DDD nao cadastrado")
