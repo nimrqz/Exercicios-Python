ef5cc5429afa3a2edee73e21de6c3473b61fc707
Nicolas Marcelino <nicolas.marcelino@exata.it>
2026-02-13 20:21:05 -0300

    exercicio 1051 Imposto de renda.py

--- 1051 Imposto de renda.py
+++ 1051 Imposto de renda.py
@@ -4,21 +4,13 @@ if sal >= 0 and sal <= 2000.00:
     print("Isento")
     exit()
     
-if sal >= 2000.01 and sal >= 3000.00:
-    salposto = sal - 2000
-    salnovo = (salposto*0.08)
-    print(f"R$ {salnovo}")
-    exit()
+if sal >= 2000.01 and sal <= 3000.00:
+    salisento = sal - 2000
+    salnovo = (salisento*0.08)
+    print(f"R$ {salnovo:.2f}")
     
-if sal >= 3000.01 and sal >= 4500.00:
-    salposto = sal - 2000
-    salnovo = (sal*0.18)
-    print(f"R$ {salnovo}")
-    exit()
-    
-if sal >= 45000.00:
-    salposto = sal - 2000
-    salnovo = (sal*0.28)
-    print(f"R$ {salnovo}")
-    exit()
-    
+if sal >= 3000.01 and sal <= 4500.00:
+    salisento = sal - 2000
+    salbaixo = (salisento - 1000)
+    salmedio = ((salbaixo*0.18) + (salisento*0.08))
+    print(f"R$ {salmedio}")
