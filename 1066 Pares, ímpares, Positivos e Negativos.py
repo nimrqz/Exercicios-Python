9aee7fb872b1be49c8a98f037cf1b7fcfa1945e9
Nicolas Marcelino <nicolas.marcelino@exata.it>
2026-02-16 09:19:42 -0300

    exercicio 1067 numeros impares

--- 1066 Pares, ímpares, Positivos e Negativos.py
+++ 1066 Pares, ímpares, Positivos e Negativos.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 positivos = 0
 negativos = 0
 pares = 0
@@ -16,30 +15,7 @@ for i in range(5):
     if valor % 2 == 1:
         impar += 1
 
-print(f"{pares} valor(es) pares(es)")
+print(f"{pares} valor(es) par(es)")
 print(f"{impar} valor(es) impar(es)")
 print(f"{positivos} valor(es) positivo(s)")
 print(f"{negativos} valor(es) negativo(s)")
-=======
-positivos = 0
-negativos = 0
-pares = 0
-impar = 0
-
-for i in range(5):
-    valor = float(input())
-    
-    if valor > 0:
-        positivos += 1
-    if valor < 0:
-        negativos += 1
-    if valor % 2 == 0:
-        pares += 1
-    if valor % 2 == 1:
-        impar += 1
-
-print(f"{pares} valor(es) pares(es)")
-print(f"{impar} valor(es) impar(es)")
-print(f"{positivos} valor(es) positivo(s)")
-print(f"{negativos} valor(es) negativo(s)")
->>>>>>> 322bd096308664fac43211a0c0fa41c63f422951
---
+++ 1067 Números Ímpares.py
@@ -0,0 +1,5 @@
+x = int(input())
+
+for i in range(1, x):
+    if i % 2 == 1:
+        print(i)
