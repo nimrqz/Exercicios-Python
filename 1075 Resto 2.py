a54713fa32cdac35e9c7344812bfa2ae2fb10558
Nicolas Marcelino <nicolasmarcelino1212@gmail.com>
2026-02-18 08:32:22 -0300

    exercicio 1075

--- 1075 Resto 2.py
+++ 1075 Resto 2.py
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 n = int(input())
 
 for i in range(1, 10000):
     if i % n == 2:
-=======
 n = int(input())
 
 for i in range(1, 10000):
     if i % n == 2:
->>>>>>> e5eca5b80cade3e518faf5c46777c88c5a7d53af
         print(i)
